25bf34874a0705782f1c22e98f9d2164728cc6fe
Yilin Mo (Michael) <moyilin276@gmail.com>
2021-08-11 11:49:44 +0800

    Create grid.h

---
+++ grid.h
@@ -0,0 +1,27 @@
+#ifndef GRID_H
+#define GRID_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include "cell.h"
+
+struct Grid {
+    string infile;
+    int score;
+    int level;
+    char curblock;
+    char nextblock;
+    std::vector<std::pair<int, int>>blockpos;
+    vector<Cell*>grid;
+    Grid();
+    ~Grid();
+    int setDefault();
+    void left();
+    void right();
+    void down();
+    void drop();
+    void compScore();
+};
+
+#endif
